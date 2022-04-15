a7ed669bea0601e87ccc93f96e736fc18f0cdcb5
Abdullah Bin Rowshan <abdullahbinrowshan@gmail.com>
2022-04-16 00:08:38 +0600

    Initialize the project

---
+++ src/firebase.init.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBKcElFO-sZUHdTVyTKIuqfnJ-FhyHoh34",
+  authDomain: "router-firebase-integrat-adcc1.firebaseapp.com",
+  projectId: "router-firebase-integrat-adcc1",
+  storageBucket: "router-firebase-integrat-adcc1.appspot.com",
+  messagingSenderId: "847815044164",
+  appId: "1:847815044164:web:d51a7953225d1461fea011"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app; 
